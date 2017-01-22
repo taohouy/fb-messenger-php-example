2421059f74df1618eab2747d27bc4088e44a1302
taohouy <taohouy@gmail.com>
2017-01-22 12:37:05 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'verify_token' => '',
+    'verify_token' => 'EAAFiLqqrKJcBAGYuiZAVlEGNtuSZBFnuBcD24mc4a2SrfxpaguVE5IZAwxZAJiFSJvY3IPtOV8LqxMx6OGwu8ZABBs7eJhmZCQ8UhsCEftOj2wOPsk0X1c3DRuiTwDFlk1RZB7hRZAJxSXe2cVQa5ZBFZAs8Rb37EUNCIC4U8wc2ZAYCAZDZD',
     'token' => ''
 ];
